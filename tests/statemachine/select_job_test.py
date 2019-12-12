d801b4d3d089699a52921fbeb090e43187f16d0b
Livio Bieri <livio@livio.li>
2019-12-12 11:48:26 +0800

    feat: adds test for job accepted on_message

--- tests/statemachine/select_job_test.py
+++ tests/statemachine/select_job_test.py
@@ -1,21 +1,22 @@
 import json
-from pathlib import Path
 from queue import Queue
-from urllib.error import HTTPError
 
 import pytest
 from pysm import Event
 
 from upparat.config import settings
-from upparat.events import SELECT_JOB_INTERRUPTED
-from upparat.events import MQTT_EVENT_TOPIC
+from upparat.events import JOB_SELECTED
 from upparat.events import MQTT_EVENT_PAYLOAD
+from upparat.events import MQTT_EVENT_TOPIC
 from upparat.events import MQTT_MESSAGE_RECEIVED
+from upparat.events import SELECT_JOB_INTERRUPTED
+from upparat.jobs import describe_job_execution_response
+from upparat.jobs import Job
+from upparat.jobs import JOB_ACCEPTED
+from upparat.jobs import JOB_MESSAGE
+from upparat.jobs import JOB_REJECTED
 from upparat.jobs import JobProgressStatus
 from upparat.jobs import JobStatus
-from upparat.jobs import describe_job_execution_response
-from upparat.jobs import JOB_REJECTED
-from upparat.jobs import JOB_MESSAGE
 from upparat.statemachine import UpparatStateMachine
 from upparat.statemachine.select_job import SelectJobState
 
@@ -116,18 +117,18 @@ def test_more_than_one_job_in_progress(select_job_state, create_enter_event, moc
     # as failed an have no pending jobs
     published_event = inbox.get_nowait()
     assert published_event.name == SELECT_JOB_INTERRUPTED
-    assert state.current_job_id == None
+    assert state.current_job_id is None
 
     # check that all we mark all as failed via mqtt
     assert mqtt_client.publish.call_count == 2
     assert mqtt_client.publish.call_args_list == [
         mocker.call(
             f"$aws/things/bobby/jobs/{job_id_1}/update",
-            f'{{"status": "{JobStatus.FAILED.value}", "statusDetails": {{"state": "{JobProgressStatus.ERROR_MULTIPLE_IN_PROGRESS.value}", "message": "More than one job IN PROGRESS: 1, 2"}}}}',
+            f'{{"status": "{JobStatus.FAILED.value}", "statusDetails": {{"state": "{JobProgressStatus.ERROR_MULTIPLE_IN_PROGRESS.value}", "message": "More than one job IN PROGRESS: 1, 2"}}}}',  # noqa
         ),
         mocker.call(
             f"$aws/things/bobby/jobs/{job_id_2}/update",
-            f'{{"status": "{JobStatus.FAILED.value}", "statusDetails": {{"state": "{JobProgressStatus.ERROR_MULTIPLE_IN_PROGRESS.value}", "message": "More than one job IN PROGRESS: 1, 2"}}}}',
+            f'{{"status": "{JobStatus.FAILED.value}", "statusDetails": {{"state": "{JobProgressStatus.ERROR_MULTIPLE_IN_PROGRESS.value}", "message": "More than one job IN PROGRESS: 1, 2"}}}}',  # noqa
         ),
     ]
 
@@ -168,3 +169,50 @@ def test_on_message_rejected_job(select_job_state, create_mqtt_message_event):
 
     published_event = inbox.get_nowait()
     assert published_event.name == SELECT_JOB_INTERRUPTED
+
+
+def test_on_message_accepted_job(select_job_state, create_mqtt_message_event):
+    state, inbox, mqtt_client, _ = select_job_state
+
+    state.current_job_id = "42"
+    settings.broker.thing_name = "bobby"
+    status = JobStatus.IN_PROGRESS.value
+    file_url = "https://foo.bar/baz.bin"
+    version = "1.1.0"
+    force = "yes"
+    meta = "_meta_"
+    status_details = "_details_"
+
+    event = create_mqtt_message_event(
+        describe_job_execution_response(
+            settings.broker.thing_name, state.current_job_id, state_filter=JOB_ACCEPTED
+        ),
+        {
+            "execution": {
+                "jobId": state.current_job_id,
+                "status": status,
+                "statusDetails": status_details,
+                "jobDocument": {
+                    "version": version,
+                    "file": file_url,
+                    "force": force,
+                    "meta": meta,
+                },
+            }
+        },
+    )
+
+    state.on_message(None, event)
+
+    published_event = inbox.get_nowait()
+    assert published_event.name == JOB_SELECTED
+
+    job = published_event.cargo["job"]
+    assert type(job) == Job
+    assert job.id_ == state.current_job_id
+    assert job.status == status
+    assert job.status_details == status_details
+    assert job.file_url == file_url
+    assert job.version == version
+    assert job.force is True
+    assert job.meta == meta
